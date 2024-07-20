0f15000ed46c0d5cb6f225ccc075d2f05fc50664
corbio <edarnand@edenschool.fr>
2024-07-20 16:38:21 +0200

    enum done

---
+++ enum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include "enum.h"
+
+int enum_test()
+{
+    Volume volume = FAIBLE;
+
+    printf("Volume: %s\n", volume);
+
+    return 0;
+}
---
+++ enum.h
@@ -0,0 +1,7 @@
+typedef enum Volume Volume;
+enum Volume
+{
+    FAIBLE = 0,
+    MOYEN = 50,
+    FORT = 100
+};
