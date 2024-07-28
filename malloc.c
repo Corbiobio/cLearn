b6be80d9a07147aa9fe06f837c204d66b5fccf6c
Corbiobio <edarnand@edenschool.fr>
2024-07-28 21:41:53 +0200

    malloc done

---
+++ malloc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    int *memory = NULL;
+
+    memory = malloc(sizeof(int));
+    int test = 0;
+    if (memory == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        exit(0);
+    }
+
+    *memory = 10;
+
+    printf("%d", *memory);
+
+    free(memory);
+
+    return 0;
+}
